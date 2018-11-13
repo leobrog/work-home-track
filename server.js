d453c4fedad5b1c84ed7fbc38c490551fd5b9f37
Leonardo <l.brognoli93@gmail.com>
2018-11-13 14:22:29 -0200

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3000
+const {mongoose} = require('./lib/db/mongoose')
+
+app.use(bodyParser.json())
+
+app.get("/log", (req, res) => {
+    const eventlogger = require('./lib/services/eventlogger')()
+    eventlogger.logEvent(req.query.type).then(result => {
+        res.send(result)
+    }).catch(err => {
+        res.send(err)
+    })
+})
+
+app.get("/history", (req, res) => {
+    const eventlogger = require('./lib/services/eventlogger')()
+    eventlogger.getHistory().then(result => {
+        res.send(result)
+    }).catch(err => {
+        res.send(err)
+    }) 
+})
+
+app.listen(port, () => {
+    console.log(`server up in port ${port}.`)
+})
