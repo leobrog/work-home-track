2d526daf33006c0eac0ae9d2ba8792c6c794ca1f
Leonardo <l.brognoli93@gmail.com>
2018-11-14 19:55:36 -0200

    Refactor, history with table, cluster, etc...

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const cluster = require("cluster")
 
-const app = express()
-const port = process.env.PORT || 3000
-const {mongoose} = require('./lib/db/mongoose')
+if( cluster.isMaster ){
+    console.log(`Master ${process.pid} is running`);
+    // Fork workers.
+    for (let i = 0; i < 3; i++) {
+        cluster.fork();
+    }
 
-app.use(bodyParser.json())
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        cluster.fork()
+    });
+} else {
 
-app.get("/log", (req, res) => {
-    const eventlogger = require('./lib/services/eventlogger')()
-    eventlogger.logEvent(req.query.type).then(result => {
-        res.send(JSON.stringify(result, null, 2))
-    }).catch(err => {
-        res.send(err)
+    const express = require('express')
+    const bodyParser = require('body-parser')
+
+    const app = express()
+    const {mongoose} = require('./lib/db/mongoose')
+
+    const services = require('./lib/routes/services')
+
+    app.set("views", "./lib/views")
+    app.set("view engine", "pug")
+
+    app.use("/services", services())
+
+    app.listen(process.env.PORT, () => {
+        console.log(`server up in port ${process.env.PORT}.`)
+        // console.log(process.env)
     })
-})
 
-app.get("/history", (req, res) => {
-    const eventlogger = require('./lib/services/eventlogger')()
-    eventlogger.getHistory().then(result => {
-        res.send(JSON.stringify(result, null, 2))
-    }).catch(err => {
-        res.send(err)
-    }) 
-})
+    console.log(`Worker #${process.pid} started`)
+}
 
-app.listen(port, () => {
-    console.log(`server up in port ${port}.`)
-})
